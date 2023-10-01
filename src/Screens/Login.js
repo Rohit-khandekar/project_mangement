cba7ac654563cae94103747d0644b619bccc6bb3
Pratibha <pratibhabansode91@gmail.com>
2023-10-01 20:27:23 +0530

    changes

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -15,51 +15,100 @@ import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import loginbg from '../utills/loginbg.svg';
 import Logo from '../utills/Logo.svg';
 import { useNavigate } from 'react-router-dom';
+import { Client, Account } from 'appwrite';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const [message, setMessage] = useState('');
-
+  const [errors, setErrors] = useState({ email: '', password: '' });
   const navigate = useNavigate();
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    if (isValidEmail(email) && isValidPassword(password)) {
-      // Perform your authentication logic here
-      // For example, you can send a request to your backend API for authentication
-      // If successful, navigate to the desired route, else show an error message
-      navigate('/dashboard'); // Navigating to the dashboard route for demonstration
-    } else {
-      setEmailError(!isValidEmail(email));
-      setPasswordError(!isValidPassword(password));
-      setMessage('Invalid email or password');
-    }
-  };
-
-  const isValidEmail = (email) => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-  };
-
-  const isValidPassword = (password) => {
-    return password.length >= 5;
-  };
-
-  const toggleShowPassword = () => {
+  const handleTogglePassword = () => {
     setShowPassword(!showPassword);
   };
 
+  const validateEmail = (value) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(value);
+  };
+
+  const validatePassword = (value) => {
+    return value.length >= 6;
+  };
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      email: validateEmail(value) ? '' : 'Invalid email address',
+    }));
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      password: validatePassword(value) ? '' : 'Password must be at least 6 characters long',
+    }));
+  };
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+  
+    const client = new Client();
+    const account = new Account(client);
+  
+    client
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject('651525be318d3396ab36');
+  
+    const emailValid = validateEmail(email);
+    const passwordValid = validatePassword(password);
+  
+    setErrors({
+      email: emailValid ? '' : 'Invalid email address',
+      password: passwordValid ? '' : 'Password must be at least 6 characters long',
+    });
+  
+    if (emailValid && passwordValid) {
+      try {
+        const promise = account.createEmailSession(email, password);
+  
+        promise.then(
+          function (response) {
+            console.log('Success', response);
+            navigate('/add-project'); // Navigate to /add-project on success
+          },
+          function (error) {
+            console.log('Error:', error);
+            console.error('Invalid credentials. Please check your inputs.');
+            setErrors((prevErrors) => ({
+              ...prevErrors,
+              email: 'Invalid email or password',
+              password: 'Invalid email or password',
+            }));
+          }
+        );
+  
+        console.log('Appwrite is connected');
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  
+  
   return (
-    <Box bgImg={loginbg} position="absolute" bgRepeat="no-repeat" w={{ base: "100%", md: "calc(100% - 80px)" }} >
-      <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"} mt={"20px"} p={"20px"} >
-        <Box >
+    <Box bgImg={loginbg} position="absolute" bgRepeat="no-repeat" w={{ base: '100%', md: 'calc(100% - 80px)' }}>
+      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} mt={'20px'} p={'20px'}>
+        <Box>
           <Image src={Logo} alt="logo" />
         </Box>
-        <Text color={"white"} fontSize={"20px"}>Online Project Management</Text>
+        <Text color={'white'} fontSize={'20px'}>
+          Online Project Management
+        </Text>
       </Box>
       <Box
         width="325px"
@@ -69,66 +118,52 @@ const Login = () => {
         mx="auto"
         borderWidth={1}
         borderRadius="15px"
-        boxShadow={"rgba(0, 0, 0, 0.35) 0px 5px 15px"}
-        mb={"100px"}
+        boxShadow={'rgba(0, 0, 0, 0.35) 0px 5px 15px'}
+        mb={'100px'}
       >
-        <Text fontSize={"20px"} textAlign={"center"} p={5}>
+        <Text fontSize={'20px'} textAlign={'center'} p={5}>
           Login To Get Started
         </Text>
-        <FormControl isInvalid={emailError}>
-          <FormLabel color={emailError ? 'red.500' : undefined}>Email</FormLabel>
-          <Input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            borderColor={emailError ? 'red.500' : undefined}
-          />
-          {emailError && (
-            <Text mt={2} color="red.500" textAlign="left">
-              Email is required
-            </Text>
-          )}
+        <FormControl isInvalid={errors.email}>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" placeholder="Enter your email" value={email} onChange={handleEmailChange} />
+          {errors.email && <Text mt={2} color="red.500" textAlign="left">{errors.email}</Text>}
         </FormControl>
 
-        <FormControl mt={4} isInvalid={passwordError}>
-          <FormLabel color={passwordError ? 'red.500' : undefined}>Password</FormLabel>
+        <FormControl mt={4} isInvalid={errors.password}>
+          <FormLabel>Password</FormLabel>
           <InputGroup>
             <Input
               type={showPassword ? 'text' : 'password'}
               placeholder="Enter your password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              borderColor={passwordError ? 'red.500' : undefined}
+              onChange={handlePasswordChange}
             />
             <InputRightElement>
               <IconButton
                 aria-label={showPassword ? 'Hide password' : 'Show password'}
                 icon={showPassword ? <ViewOffIcon /> : <ViewIcon />}
-                onClick={toggleShowPassword}
+                onClick={handleTogglePassword}
                 variant="ghost"
               />
             </InputRightElement>
           </InputGroup>
-          {passwordError && (
-            <Text mt={2} color="red.500" textAlign="left">
-              Password is required
-            </Text>
-          )}
+          {errors.password && <Text mt={2} color="red.500" textAlign="left">{errors.password}</Text>}
         </FormControl>
 
-        <Box w={"100%"} display={"flex"} justifyContent={"center"}>
-          <Button m={4} borderRadius={"20px"} p={"0px"} w={"200px"} colorScheme="teal" bg={"#035fb2"} color={"white"} onClick={handleLogin}>
+        <Box w={'100%'} display={'flex'} justifyContent={'center'}>
+          <Button
+            m={4}
+            borderRadius={'20px'}
+            p={'0px'}
+            w={'200px'}
+            colorScheme="teal"
+            bg={'#035fb2'}
+            color={'white'}
+            onClick={handleSubmission}
+          >
             Login
           </Button>
-        </Box>
-
-        <Box w={"100%"} display={"flex"} justifyContent={"center"}>
-          {message && (
-            <Text mt={2} color="red.500" textAlign="left">
-              {message}
-            </Text>
-          )}
         </Box>
       </Box>
     </Box>
